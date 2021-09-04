fb681825305b55decc5f8eeab36937245bffb329
Daniel Phan <danielphan.2003@gmail.com>
2021-09-04 19:05:04 +0700

    overlays: ignore wine wayland

--- overlays/wine.nix
+++ overlays/wine.nix
@@ -1,20 +1,18 @@
-channels: final: prev:
-let in
-{
-  wine-staging = channels.latest.wine.override {
-    wineRelease = "staging";
-    wineBuild = "wineWow";
-    callPackage = path: args: with prev;
-      let package = callPackage path args; in
-      if package ? overrideAttrs
-      then
-        (package.overrideAttrs (o: {
-          buildInputs = o.buildInputs ++ [ wayland egl-wayland libxkbcommon ];
-        })).override
-          {
-            inherit (final.sources.wine-wayland) src;
-            configureFlags = [ "--with-wayland" "--with-vulkan" "--with-vkd3d" ];
-          }
-      else package;
-  };
+channels: final: prev: {
+  # wine-staging = channels.latest.wine.override {
+  #   wineRelease = "staging";
+  #   wineBuild = "wineWow";
+  #   callPackage = path: args: with prev;
+  #     let package = callPackage path args; in
+  #     if package ? overrideAttrs
+  #     then
+  #       (package.overrideAttrs (o: {
+  #         buildInputs = o.buildInputs ++ [ wayland egl-wayland libxkbcommon ];
+  #       })).override
+  #         {
+  #           inherit (final.sources.wine-wayland) src;
+  #           configureFlags = [ "--with-wayland" "--with-vulkan" "--with-vkd3d" ];
+  #         }
+  #     else package;
+  # };
 }
