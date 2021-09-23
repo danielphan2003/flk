79ddc56112cb7a42cc9b5fed9c275f4a754fbe8b
Daniel Phan <danielphan.2003@gmail.com>
2021-09-23 08:26:04 +0700

    root: trust danie to deploy system config

--- home/users/root/default.nix
+++ home/users/root/default.nix
@@ -3,5 +3,9 @@ let user = "root"; in
 {
   # recommend using `hashedPassword`
   age.secrets.root.file = "${self}/secrets/home/users/${user}.age";
-  users.users.root.passwordFile = config.age.secrets."${user}".path;
+  users.users.root = {
+    passwordFile = config.age.secrets."${user}".path;
+    # pwease trust me root-senpai~~
+    openssh.authorizedKeys.keyFiles = [ "${self}/secrets/ssh/danie.pub" ];
+  };
 }
