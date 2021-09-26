e59fa951177f66bccb210e2a88461420d0016fe9
Daniel Phan <danielphan.2003@gmail.com>
2021-09-26 15:46:54 +0700

    netdata: refactor, use latest stable instead

--- overlays/netdata.nix
+++ overlays/netdata.nix
@@ -1,30 +1,12 @@
 channels: final: prev: {
-  netdata = with channels.latest; netdata.overrideAttrs (o:
-    let
-      go-d-plugin = callPackage ../pkgs/tools/system/netdata/go.d.plugin.nix { inherit (final) sources; };
-    in
-    {
-      inherit (final.sources.netdata) pname src version;
-      patches = lib.our.getPatches ../pkgs/tools/system/netdata;
-      buildInputs = o.buildInputs
-        ++ lib.optionals (!stdenv.isDarwin) [ protobuf ];
-      postInstall = ''
-        ln -s ${go-d-plugin}/lib/netdata/conf.d/* $out/lib/netdata/conf.d
-        ln -s ${go-d-plugin}/bin/godplugin $out/libexec/netdata/plugins.d/go.d.plugin
-      '' + lib.optionalString (!stdenv.isDarwin) ''
-        # rename this plugin so netdata will look for setuid wrapper
-        mv $out/libexec/netdata/plugins.d/apps.plugin \
-          $out/libexec/netdata/plugins.d/apps.plugin.org
-        mv $out/libexec/netdata/plugins.d/cgroup-network \
-          $out/libexec/netdata/plugins.d/cgroup-network.org
-        mv $out/libexec/netdata/plugins.d/perf.plugin \
-          $out/libexec/netdata/plugins.d/perf.plugin.org
-        mv $out/libexec/netdata/plugins.d/slabinfo.plugin \
-          $out/libexec/netdata/plugins.d/slabinfo.plugin.org
-        ${lib.optionalString (!stdenv.isDarwin) ''
-          mv $out/libexec/netdata/plugins.d/freeipmi.plugin \
-            $out/libexec/netdata/plugins.d/freeipmi.plugin.org
-        ''}
-      '';
+  netdata = with channels.latest; (netdata.override {
+    callPackage = args: deps: callPackage args (deps // {
+      buildGoModule = goArgs: buildGoModule (goArgs // {
+        inherit (final.sources.netdata-go-d-plugin) src version;
+        vendorSha256 = "sha256-kfrP8cLnVn+akA9zwx10OBJ62PAoZAkG9ZiAxN+sR5M=";
+      });
     });
+  }).overrideAttrs (_: {
+    inherit (final.sources.netdata) pname src version;
+  });
 }
