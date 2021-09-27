c0aa5c04fa087575e20a2023c8bb2d47360d71e1
Daniel Phan <danielphan.2003@gmail.com>
2021-09-27 13:19:39 +0700

    graphical/misc: init  - decouple from x server completely

---
+++ nixos/profiles/graphical/misc/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, lib, config, ... }:
+let
+  inherit (builtins) attrValues;
+  inherit (lib) mkDefault optionalAttrs;
+in
+{
+  environment.systemPackages = attrValues {
+    inherit (pkgs)
+      xdg-utils
+      nixos-icons# needed for gnome and pantheon about dialog, nixos-manual and maybe more
+      ;
+  };
+
+  xdg = {
+    autostart.enable = true;
+    menus.enable = true;
+    mime.enable = true;
+    icons.enable = true;
+  };
+
+  # The default max inotify watches is 8192.
+  # Nowadays most apps require a good number of inotify watches,
+  # the value below is used by default on several other distros.
+  boot.kernel.sysctl."fs.inotify.max_user_instances" = mkDefault 524288;
+  boot.kernel.sysctl."fs.inotify.max_user_watches" = mkDefault 524288;
+
+  fonts.enableDefaultFonts = mkDefault true;
+}
