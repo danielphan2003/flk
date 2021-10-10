6350e1315710c31a1b9b009af3672af5f872346e
Daniel Phan <danielphan.2003@gmail.com>
2021-10-11 04:04:45 +0700

    electron-ov: use symlinkJoin & fix double wrapping

--- overlays/electron.nix
+++ overlays/electron.nix
@@ -10,14 +10,15 @@ let
     ;
 
   inherit (final)
-    electronCompatFlags
     electron
     sources
     ;
 
   inherit (prev)
+    makeWrapper
     microsoft-edge-beta
     spotify-spicetified
+    symlinkJoin
     writeShellScript
     ;
 
@@ -25,7 +26,9 @@ let
     concatMapStringsSep
     concatStringsSep
     escapeShellArg
+    last
     optionals
+    splitString
     toList
     ;
 
@@ -50,45 +53,62 @@ let
 
   flagsCommand = concatStringsSep " ";
 
-  # TODO: prevent double wrapping
+  compatScript = flags: writeShellScript "electron-compat-flags.sh" ''
+    if [[ -n $WAYLAND_DISPLAY ]]
+    then
+      echo ${flagsCommand flags}
+    else
+      echo ""
+    fi
+  '';
+
+  electronCompatFlags = flags: "$(${compatScript flags})";
+
   patchElectron = bin: { flags ? defaultFlags }:
     let
-      wrapper = bin': ''
-        wrapProgram ${bin'} \
-          --add-flags ${escapeShellArg (electronCompatFlags flags)}
-      '';
+      # Very hacky, but works nonetheless
+      # substitutePhase = "substituteInPlace ${bin'} --replace '$@' '$@\" \"\" \$(${escapeShellArg (compatScript flags)}) \"'";
+      wrapper = bin':
+        let
+          binName = last (splitString "/" bin');
+        in
+        ''
+          mkdir -p $out/.bin-wrapped
+          cp --copy-contents ${bin'} $out/.bin-wrapped/${binName}
+          rm ${bin'}
+          makeWrapper $out/.bin-wrapped/${binName} ${bin'} \
+            --add-flags ${escapeShellArg (electronCompatFlags flags)}
+        '';
     in
     concatMapStringsSep "\n" (x: wrapper x) (toList bin);
 
-  patchElectronApplication = pkg: bin: { flags ? defaultFlags, patchStage ? "postFixup", ... }@args:
-    pkg.overrideAttrs (o: args // {
-      "${patchStage}" = (o."${patchStage}" or "")
-        + patchElectron bin { inherit flags; };
+  patchElectronApplication = pkg: bin: { flags ? defaultFlags }:
+    (symlinkJoin {
+      inherit (pkg) name;
+      paths = [ pkg ];
+      buildInputs = [ makeWrapper ];
+      postBuild = patchElectron bin { inherit flags; };
+    }).overrideAttrs (_: {
+      inherit (pkg)
+        meta
+        name
+        version
+        ;
+      pname = pkg.pname or pkg.name;
     });
 
   patchBrowser = pkg: { flags ? defaultFlags }:
     pkg.override { commandLineArgs = electronCompatFlags flags; };
 in
 {
-  electronCompatFlags = flags:
-    let
-      compatScript = writeShellScript "electron-compat-flags.sh" ''
-        if [[ -n $WAYLAND_DISPLAY ]]
-        then
-          echo '${flagsCommand flags}'
-        else
-          echo ""
-        fi
-      '';
-    in
-    "$(${compatScript})";
-
   element-desktop = element-desktop.override { inherit electron; };
 
-  discord-canary = patchElectronApplication discord-canary [ "$out/bin/DiscordCanary" "$out/bin/discordcanary" ] {
-    inherit (sources.discord-canary) pname src version;
-  };
+  discord-canary = patchElectronApplication
+    (discord-canary.overrideAttrs (_: { inherit (sources.discord-canary) pname src version; }))
+    "$out/opt/DiscordCanary/DiscordCanary"
+    { };
 
+  # TODO: signal still uses xwayland
   signal-desktop = patchElectronApplication signal-desktop "$out/bin/signal-desktop" { };
 
   electron = patchElectronApplication channels.latest.electron "$out/lib/electron/electron" { };
@@ -101,8 +121,9 @@ in
 
   spotify-spicetified = patchBrowser spotify-spicetified { flags = waylandFlags; };
 
-  teams = patchElectronApplication teams "$out/bin/teams" {
-    inherit (sources.teams) src version;
-  };
+  teams = patchElectronApplication
+    (teams.overrideAttrs (_: { inherit (sources.teams) src version; }))
+    "$out/bin/teams"
+    { };
 
 }
