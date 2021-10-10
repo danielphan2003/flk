05ad3b6f604c5cb66fc57cf621c4a6c26e65144e
Daniel Phan <danielphan.2003@gmail.com>
2021-10-11 04:26:42 +0700

    eww: revamp sway workspaces

--- home/profiles/eww/config/scripts/current-workspace.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-$(swaymsg -m -t subscribe '[ "workspace" ]' | jq '.change == "focus"') && swaymsg -t get_workspaces | jq '.[] | select(.focused).num'
--- home/profiles/eww/default.nix
+++ home/profiles/eww/default.nix
@@ -1,8 +1,8 @@
 { pkgs, lib, ... }: {
   services.eww-mpris.enable = true;
   services.eww.enable = true;
-  # xdg.configFile."eww" = {
-  #   source = ./config;
-  #   # recursive = true;
-  # };
+  xdg.configFile."eww" = {
+    source = ./config;
+    # recursive = true;
+  };
 }
