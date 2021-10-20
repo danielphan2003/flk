1518d125c1b6e9e780f8fc0017d0fd8d52b0690f
Daniel Phan <danielphan.2003@gmail.com>
2021-10-20 09:10:46 +0700

    tailscale: update vendor hash

--- overlays/tailscale.nix
+++ overlays/tailscale.nix
@@ -4,7 +4,7 @@ let buildGo117Module = with channels.latest; buildGoModule.override { go = go_1_
   tailscale = channels.latest.tailscale.override {
     buildGoModule = args: buildGo117Module (args // {
       inherit (final.sources.tailscale) pname src version;
-      vendorSha256 = "sha256-8WN9BsLVYk3mb1cQkFxDoN+OynIUV1zSLcF1l8+Ijdc=";
+      vendorSha256 = "sha256-+Vk+HU4AA9ApfO8ui8xS3f6pyVvzmUxQvGeQzC0DGm8=";
     });
   };
 }
