6f51226d4b283c6c3ce4c658508bb497184318ba
Daniel Phan <danielphan.2003@gmail.com>
2021-10-24 06:56:27 +0700

    nixos/profiles/apps/base: init

---
+++ nixos/profiles/apps/base/default.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }: {
+  environment.shellAliases.dig = "doggo";
+
+  environment.systemPackages = builtins.attrValues {
+    inherit (pkgs)
+      binutils
+      bottom
+      cachix
+      coreutils
+      curl
+      direnv
+      dnsutils
+      doggo
+      fd
+      git
+      iptables
+      iputils
+      jq
+      moreutils
+      pciutils
+      rsync
+      tealdeer
+      usbutils
+      utillinux
+      wget
+      whois
+      wireguard-tools
+    ;
+  };
+}
