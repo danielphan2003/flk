1169f0a8b3c03cb895f7ddc82f86fe9da4c9edb8
Daniel Phan <danielphan.2003@gmail.com>
2021-10-24 06:57:46 +0700

    nixos/profiles: move develop pkgs to apps/develop

---
+++ nixos/profiles/apps/develop/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }: {
+  documentation.dev.enable = true;
+
+  environment.systemPackages = builtins.attrValues {
+    inherit (pkgs)
+      bc
+      file
+      gomod2nix
+      lazygit
+      less
+      manix
+      tig
+      tokei
+      rnix-lsp
+    ;
+  };
+
+}
