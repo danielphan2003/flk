84233a8171370aeefb9e2ee09ce854a68237ccc7
Daniel Phan <danielphan.2003@gmail.com>
2021-10-24 07:04:50 +0700

    nixos/profiles/cloud/spotifyd: proper secret env

--- nixos/profiles/cloud/spotifyd/default.nix
+++ nixos/profiles/cloud/spotifyd/default.nix
@@ -1,12 +1,16 @@
-{ config, self, ... }: {
-  age.secrets.spotify.file = "${self}/nixos/profiles/cloud/spotify.age";
+{ config, self, ... }: let inherit (config.age.secrets.spotify) path; in {
+  age.secrets.spotify.file = "${self}/secrets/nixos/profiles/cloud/spotify.age";
+
+  systemd.services.spotifyd = {
+    serviceConfig.EnvironmentFile = path;
+  };
 
   services.spotifyd = {
     enable = true;
     settings = {
       global = {
-        username_cmd = "source ${config.age.secrets.spotify.path} && echo $SPOTIFY_USER";
-        password_cmd = "source ${config.age.secrets.spotify.path} && echo $SPOTIFY_PASSWD";
+        username_cmd = "echo $SPOTIFY_USER";
+        password_cmd = "echo $SPOTIFY_PASSWD";
         device_name = config.networking.hostName;
         cache_path = "/var/cache/spotify";
         initial_volume = "100";
