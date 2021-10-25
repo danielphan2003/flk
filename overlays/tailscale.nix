e8aa2b3843f699036586b64bb8341d06074bcc26
Daniel Phan <danielphan.2003@gmail.com>
2021-10-25 18:53:54 +0700

    tailscale: update hash

--- overlays/tailscale.nix
+++ overlays/tailscale.nix
@@ -1,12 +1,13 @@
 channels: final: prev:
 let buildGo117Module = with channels.latest; buildGoModule.override { go = go_1_17; }; in
 {
-  tailscale = with channels.latest; callPackage ({ tailscale, hostName ? "ts-${prev.system}", customDoHPath ? null }: tailscale.override {
-    buildGoModule = args: buildGo117Module (args // {
-      inherit (final.sources.tailscale) pname src version;
-      vendorSha256 = "sha256-+Vk+HU4AA9ApfO8ui8xS3f6pyVvzmUxQvGeQzC0DGm8=";
-      patches = [ ]
-        ++
+  tailscale = with channels.latest; callPackage
+    ({ tailscale, hostName ? "ts-${prev.system}", customDoHPath ? null }: tailscale.override {
+      buildGoModule = args: buildGo117Module (args // {
+        inherit (final.sources.tailscale) pname src version;
+        vendorSha256 = "sha256-JOS+OalyrvO2Br5MrcmfCVbewdaeVXmanIAoM5eOpvQ=";
+        patches = [ ]
+          ++
           (if customDoHPath != null
           then
             [
@@ -16,6 +17,7 @@ let buildGo117Module = with channels.latest; buildGoModule.override { go = go_1_
               })
             ]
           else [ ]);
-    });
-  }) { };
+      });
+    })
+    { };
 }
