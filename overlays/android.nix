d07e9c7720b1103e7f852ae1eb6d16e7a655acc6
Daniel Phan <danielphan.2003@gmail.com>
2021-11-03 04:03:32 +0700

    waydroid: use latest

--- overlays/android.nix
+++ overlays/android.nix
@@ -1,5 +1,6 @@
 channels: final: prev: {
   waydroid = channels.latest.waydroid.overrideAttrs (_: {
+    inherit (final.sources.waydroid) src version;
     patches = prev.lib.our.getPatches ../pkgs/os-specific/linux/waydroid;
   });
 }
