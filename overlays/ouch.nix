3a482a5fb69cce25717846994491fef11587b0f4
Daniel Phan <danielphan.2003@gmail.com>
2021-11-19 15:24:08 +0700

    ouch: override latest

--- overlays/ouch.nix
+++ overlays/ouch.nix
@@ -1,6 +1,12 @@
-channels: final: prev: {
-  ouch = channels.latest.rustPlatform.buildRustPackage {
-    inherit (final.sources.ouch) pname src version cargoLock;
-    inherit (channels.latest.ouch) meta;
+channels: final: prev:
+let
+  inherit (channels.latest) ouch fenix makeRustPlatform;
+  rustPlatform = makeRustPlatform { inherit (fenix.latest) cargo rustc; };
+in
+{
+  ouch = ouch.override {
+    rustPlatform.buildRustPackage = args: rustPlatform.buildRustPackage (builtins.removeAttrs args [ "cargoSha256" ] // {
+      inherit (final.sources.ouch) pname src version cargoLock;
+    });
   };
 }
