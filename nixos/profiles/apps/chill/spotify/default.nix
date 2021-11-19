206c9b749cb72f52355fa7912aeb3d6952b74dc5
Daniel Phan <danielphan.2003@gmail.com>
2021-11-19 15:22:37 +0700

    spotify-spicetified: fix js_repl_8008 replacement

--- nixos/profiles/apps/chill/spotify/default.nix
+++ nixos/profiles/apps/chill/spotify/default.nix
@@ -15,6 +15,45 @@
           };
           customExtensions = {
             "ddt.js" = "${pkgs.dribbblish-dynamic-theme}/extensions/dribbblish-dynamic.js";
+            # "enable-devtools.js" = pkgs.writeText "enable-devtools.js" ''
+            #   // @ts-check
+
+            #   /// <reference path="../globals.d.ts" />
+
+            #   (function ChristianSpotify() {
+            #       /**
+            #       *
+            #       * @param {Spicetify.Keyboard.ValidKey} keyName
+            #       * @param {boolean} ctrl
+            #       * @param {boolean} shift
+            #       * @param {boolean} alt
+            #       * @param {(event: KeyboardEvent) => void} callback
+            #       */
+            #       function registerBind(keyName, ctrl, shift, alt, callback) {
+            #           const key = Spicetify.Keyboard.KEYS[keyName];
+
+            #           Spicetify.Keyboard.registerShortcut(
+            #               {
+            #                   key,
+            #                   ctrl,
+            #                   shift,
+            #                   alt,
+            #               },
+            #               callback
+            #           );
+            #       }
+            #       registerBind("F12", false, false, false, rotateSidebarDown);
+            #       document.addEventListener("keydown", function (e) {
+            #           if (e.which === 123) {
+            #               //F12
+            #               require("electron").remote.BrowserWindow.getFocusedWindow().webContents.toggleDevTools();
+            #           } else if (e.which === 116) {
+            #               //F5
+            #               location.reload();
+            #           }
+            #       });
+            #   })();
+            # '';
           };
           enabledCustomApps = [
             "lyrics-plus"
@@ -32,7 +71,7 @@
           extraConfig = ''
             [Patch]
             xpui.js_find_8008 = ,(\w+=)32,
-            xpui.js_repl_8008 = ,''${1}56,
+            xpui.js_repl_8008 = ,''${1}58,
           '';
         }
     else spotify-tui)
