54f762ebf03e6eb280df952dc9ea0ed8d0d7cb3a
Daniel Phan <danielphan.2003@gmail.com>
2021-11-23 06:00:43 +0700

    graphical/misc: force inotify watches and instances

--- nixos/profiles/graphical/misc/default.nix
+++ nixos/profiles/graphical/misc/default.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, ... }:
 let
   inherit (builtins) attrValues;
-  inherit (lib) mkDefault optionalAttrs;
+  inherit (lib) mkDefault mkForce optionalAttrs;
 in
 {
   environment.systemPackages = attrValues {
@@ -21,8 +21,8 @@ in
   # The default max inotify watches is 8192.
   # Nowadays most apps require a good number of inotify watches,
   # the value below is used by default on several other distros.
-  boot.kernel.sysctl."fs.inotify.max_user_instances" = mkDefault 524288;
-  boot.kernel.sysctl."fs.inotify.max_user_watches" = mkDefault 524288;
+  boot.kernel.sysctl."fs.inotify.max_user_instances" = mkForce 524288;
+  boot.kernel.sysctl."fs.inotify.max_user_watches" = mkForce 524288;
 
   fonts.enableDefaultFonts = mkDefault true;
 }
