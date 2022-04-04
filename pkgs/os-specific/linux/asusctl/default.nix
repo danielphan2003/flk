9ea6c4f04383e6ad375b4cba3b254f08c87fc9e6
Daniel Phan <34669737+danielphan2003@users.noreply.github.com>
2022-04-04 13:37:54 +0700

    asusctl: bump version

--- pkgs/os-specific/linux/asusctl/default.nix
+++ pkgs/os-specific/linux/asusctl/default.nix
@@ -1,52 +1,40 @@
 {
   stdenv,
   lib,
-  fetchFromGitLab,
+  sources,
   rustPlatform,
   rust,
   pkg-config,
   libudev,
 }:
-with rustPlatform;
-  buildRustPackage rec {
-    pname = "asusctl";
-    version = "4.0.7";
+rustPlatform.buildRustPackage {
+  inherit (sources.asusctl) pname src version cargoLock;
 
-    src = fetchFromGitLab {
-      owner = "asus-linux";
-      repo = "asusctl";
-      rev = version;
-      hash = "sha256-pAZO+cEzy9OlheZ96HON8ECR1YZaf/k9o4tnXKx5oY8=";
-    };
+  nativeBuildInputs = [pkg-config];
+  buildInputs = [libudev];
 
-    cargoLock = {
-      lockFile = "${src}/Cargo.lock";
-    };
+  doCheck = false;
 
-    nativeBuildInputs = [pkg-config];
-    buildInputs = [libudev];
+  postPatch = ''
+    # The build is placed under target/x86_64-unknown-linux-gnu/release, while Makefile expects it under
+    # target/release. We'll create target/release to be compatible with Makefile.
+    substituteInPlace Makefile \
+      --replace "./target/release/" "./target/${rust.toRustTargetSpec stdenv.hostPlatform}/release/"
+  '';
 
-    doCheck = false;
+  # Avoid referring to systemd from udev rules file to avoid dependency on systemd.
+  # substituteInPlace data/asusd.rules \
+  #   --replace "systemctl" "${systemd}/bin/systemctl"
 
-    postPatch = ''
-      # The build is placed under target/x86_64-unknown-linux-gnu/release, while Makefile expects it under
-      # target/release. We'll create target/release to be compatible with Makefile.
-      substituteInPlace Makefile \
-        --replace "./target/release/" "./target/${rust.toRustTargetSpec stdenv.hostPlatform}/release/"
-    '';
+  installPhase = ''
+    make install DESTDIR="$out" prefix=""
+  '';
 
-    # Avoid referring to systemd from udev rules file to avoid dependency on systemd.
-    # substituteInPlace data/asusd.rules \
-    #   --replace "systemctl" "${systemd}/bin/systemctl"
-
-    installPhase = ''
-      make install DESTDIR="$out" prefix=""
-    '';
-
-    meta = with lib; {
-      description = "A control daemon, CLI tools, and a collection of crates for interacting with ASUS ROG laptops.";
-      homepage = https://gitlab.com/asus-linux/asusctl;
-      license = with licenses; [mpl20];
-      platforms = platforms.linux;
-    };
-  }
+  meta = with lib; {
+    description = "A control daemon, CLI tools, and a collection of crates for interacting with ASUS ROG laptops.";
+    homepage = "https://gitlab.com/asus-linux/asusctl";
+    license = with licenses; [mpl20];
+    platforms = platforms.linux;
+    maintainers = with maintainers; [danielphan2003];
+  };
+}
